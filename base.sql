d5f320b4a72271f7d2ef92d79475c14ed5039cbc
MatiasJF <matias.jackson.f@gmail.com>
2023-09-28 18:06:32 +0200

    venv casas , dockerfile , sql y ipynb

--- base.sql
+++ base.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE IF NOT EXISTS pbd1;
+
 USE pbd1;
+
 CREATE TABLE IF NOT EXISTS casas (
     id INT AUTO_INCREMENT PRIMARY KEY,
     longitude DECIMAL(9,6),
@@ -7,17 +9,9 @@ CREATE TABLE IF NOT EXISTS casas (
     housing_median_age DECIMAL(4,1),
     total_rooms DECIMAL(7,1),
     total_bedrooms DECIMAL(7,1),
-    population DECIMAL(7,1),
+    populations DECIMAL(7,1),
     households DECIMAL(7,1),
     median_income DECIMAL(7,4),
     median_house_value DECIMAL(10,2),
     ocean_proximity VARCHAR(50)
 );
-
-LOAD DATA INFILE '/PBDI_data_1_housing.csv'
-INTO TABLE casas
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES;
-
-SELECT * FROM casas;
