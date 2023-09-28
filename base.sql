f6e68a061597cdf9780e2c088d0ef1949cfe4e65
MatiasJF <matias.jackson.f@gmail.com>
2023-09-28 12:10:44 +0200

    generado de archivos y dockerflie

--- base.sql
+++ base.sql
@@ -1,15 +1,23 @@
-CREATE DATABASE BIG_DATA;
+CREATE DATABASE IF NOT EXISTS pbd1;
+USE pbd1;
+CREATE TABLE IF NOT EXISTS casas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    longitude DECIMAL(9,6),
+    latitude DECIMAL(8,6),
+    housing_median_age DECIMAL(4,1),
+    total_rooms DECIMAL(7,1),
+    total_bedrooms DECIMAL(7,1),
+    population DECIMAL(7,1),
+    households DECIMAL(7,1),
+    median_income DECIMAL(7,4),
+    median_house_value DECIMAL(10,2),
+    ocean_proximity VARCHAR(50)
+);
 
-CREATE TABLE HOUSING(
-    Longitud float (7),
-    Latitud float (7),
-    Housing_median_age float (5),
-    Total_rooms float,
-    Total_bethrooms float,
-    Populations float,
-    House_holds float,
-    Median_Income float,
-    Median_House_Value float,
-    Ocean_Proximity varchar (60),
-    PRIMARY KEY (Longitud, Latitud)
-    )
+LOAD DATA INFILE '/PBDI_data_1_housing.csv'
+INTO TABLE casas
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+SELECT * FROM casas;
